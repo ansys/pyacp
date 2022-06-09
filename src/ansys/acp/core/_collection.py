4b40a31c62e20830623bb085dbccf500d085474e
Dominik Gresch <greschd@users.noreply.github.com>
2022-06-09 11:18:36 +0200

    Add deletion methods to Collection (#51)
    Add the deleting mapping methods to the Collection interface.
    The setter methods are still missing, but their signatures are added
    as a comment.

--- src/ansys/acp/core/_collection.py
+++ src/ansys/acp/core/_collection.py
@@ -10,20 +10,49 @@ class Collection(Generic[ValueT]):
         self,
         list_method: Callable[[], Sequence[BasicInfo]],
         constructor: Callable[[str], ValueT],
+        delete_method: Callable[[BasicInfo], None],
     ):
         # TODO: should the list method return instantiated objects, or
         # do we want to work with the bare protobuf responses here?
         self._list_method = list_method
         self._constructor = constructor
+        self._delete_method = delete_method
 
     def __iter__(self) -> Iterator[str]:
         yield from (obj.id for obj in self._list_method())
 
     def __getitem__(self, key: str) -> ValueT:
+        info = self._get_info_by_id(key)
+        return self._constructor(info.resource_path.value)
+
+    def _get_info_by_id(self, key: str) -> BasicInfo:
         for obj in self._list_method():
             if obj.id == key:
-                return self._constructor(obj.resource_path.value)
+                return obj
         raise KeyError(f"No object with ID '{key}' found.")
+
+    # def __setitem__(self, key: str, value: ValueT) -> None:
+    #     raise NotImplementedError()
+
+    # def update(self, other=(), /, **kwds):
+    #     raise NotImplementedError()
+
+    # def setdefault(self, key, default=None):
+    #     raise NotImplementedError()
+
+    def __delitem__(self, key: str) -> None:
+        info = self._get_info_by_id(key)
+        self._delete_method(info)
+
+    def clear(self) -> None:
+        for info in self._list_method():
+            self._delete_method(info)
+
+    # def pop(self, key):
+    #     raise NotImplementedError()
+
+    # def popitem(self, key):
+    #     raise NotImplementedError()
 
     def values(self) -> Iterator[ValueT]:
         return (self._constructor(obj.resource_path.value) for obj in self._list_method())
