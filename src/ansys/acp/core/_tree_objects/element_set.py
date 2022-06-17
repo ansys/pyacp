7a0959a0b69ca6589e60f0ce1735a48f880cc6fa
Dominik Gresch <dominik.gresch@ansys.com>
2022-06-17 14:45:27 +0200

    Expose ElementSet properties

--- src/ansys/acp/core/_tree_objects/element_set.py
+++ src/ansys/acp/core/_tree_objects/element_set.py
@@ -1,11 +1,13 @@
 from __future__ import annotations
 
 from functools import lru_cache
+from typing import Container
 
 from ansys.api.acp.v0.element_set_pb2 import CreateElementSetRequest, ElementSetInfo
 from ansys.api.acp.v0.element_set_pb2_grpc import ElementSetStub
 
-from .._grpc_helpers.property_helper import grpc_data_property
+from .._grpc_helpers.property_helper import grpc_data_property, grpc_data_property_read_only
+from .._utils.array_conversions import to_1D_int_array, to_tuple_from_1D_array
 from .base import CreatableTreeObject
 
 __all__ = ["ElementSet"]
@@ -16,15 +18,26 @@ class ElementSet(CreatableTreeObject):
     OBJECT_INFO_TYPE = ElementSetInfo
     CREATE_REQUEST_TYPE = CreateElementSetRequest
 
-    def __init__(self, name: str = "Element Set"):
+    def __init__(
+        self,
+        name: str = "Element Set",
+        middle_offset: bool = False,
+        element_labels: Container[int] = tuple(),
+    ):
         """Instantiate an Element Set.
 
         Parameters
         ----------
         name :
             The name of the Element Set.
+        middle_offset :
+            TODO
+        element_labels :
+            TODO
         """
         super().__init__(name=name)
+        self.middle_offset = middle_offset
+        self.element_labels = element_labels
 
     # Mypy doesn't like this being a property, see https://github.com/python/mypy/issues/1362
     @lru_cache(maxsize=1)
@@ -32,3 +45,11 @@ class ElementSet(CreatableTreeObject):
         return ElementSetStub(self._channel)
 
     id = grpc_data_property("info.id")
+
+    locked = grpc_data_property_read_only("properties.locked")
+    middle_offset = grpc_data_property("properties.middle_offset")
+    element_labels = grpc_data_property(
+        "properties.element_labels",
+        from_protobuf=to_tuple_from_1D_array,
+        to_protobuf=to_1D_int_array,
+    )
--- src/ansys/acp/core/_utils/array_conversions.py
+++ src/ansys/acp/core/_utils/array_conversions.py
@@ -1,13 +1,17 @@
-from typing import Tuple
+from typing import Any, Tuple, Union
 
-from ansys.api.acp.v0.array_types_pb2 import DoubleArray
+from ansys.api.acp.v0.array_types_pb2 import DoubleArray, IntArray
 
 
 def to_1D_double_array(data: Tuple[float, ...]) -> DoubleArray:
     return DoubleArray(shape=[len(data)], data=data)
 
 
-def to_tuple_from_1D_array(double_array: DoubleArray) -> Tuple[float, ...]:
-    if not len(double_array.shape) == 1:
-        raise RuntimeError(f"Cannot convert {len(double_array.shape)}-dimensional array to tuple!")
-    return tuple(double_array.data)
+def to_1D_int_array(data: Tuple[int, ...]) -> IntArray:
+    return IntArray(shape=[len(data)], data=data)
+
+
+def to_tuple_from_1D_array(array: Union[IntArray, DoubleArray]) -> Tuple[Any, ...]:
+    if not len(array.shape) == 1:
+        raise RuntimeError(f"Cannot convert {len(array.shape)}-dimensional array to tuple!")
+    return tuple(array.data)
