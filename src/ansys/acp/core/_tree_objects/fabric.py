d35eff6b2125ca0723ed00446437931696ac32d9
Rene Roos <rene.roos@ansys.com>
2022-06-23 14:01:41 +0200

    comment Material property of Fabric because linked object as property is not supported yet.

--- src/ansys/acp/core/_tree_objects/fabric.py
+++ src/ansys/acp/core/_tree_objects/fabric.py
@@ -9,12 +9,10 @@ from ansys.api.acp.v0.drop_off_material_pb2 import MaterialHandlingType as Cutof
 from ansys.api.acp.v0.ply_material_pb2 import DrapingMaterialType
 
 from .._grpc_helpers.property_helper import grpc_data_property, grpc_data_property_read_only
-from .._grpc_helpers.linked_object_list import define_linked_object_list
 from .._utils.enum_conversions import status_type_to_string
-from .._utils.linked_objec_conversion import from_rp_to_tree_object, from_tree_object_to_rp
 from .base import CreatableTreeObject
 
-from .material import Material
+#from .material import Material
 
 __all__ = ["Fabric"]
 
@@ -52,7 +50,7 @@ class Fabric(CreatableTreeObject):
     def __init__(
         self,
         name: str = "Fabric",
-        material: Material = None,
+        #material: Material = None,
         thickness: float = 0.,
         area_price: float = 0.,
         ignore_for_postprocessing: bool = False,
@@ -63,7 +61,7 @@ class Fabric(CreatableTreeObject):
     ):
         super().__init__(name=name)
 
-        self.material = material
+        #self.material = material
         self.thickness = thickness
         self.area_price = area_price
         self.ignore_for_postprocessing = ignore_for_postprocessing
@@ -89,7 +87,5 @@ class Fabric(CreatableTreeObject):
 
     draping_ud_coefficient = grpc_data_property("properties.draping_ud_coefficient")
 
-    material = grpc_data_property("properties.material",
-                                  to_protobuf=from_rp_to_tree_object())
-
+    #material = TBD
 
