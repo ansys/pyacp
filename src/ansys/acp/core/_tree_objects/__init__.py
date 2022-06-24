3780b76b02dd0f33fb8c1a80743bc6576739accc
Rene Roos <rene.roos@ansys.com>
2022-06-24 10:57:25 +0200

    material was missing in tree_objects/__init__.py

--- src/ansys/acp/core/_tree_objects/__init__.py
+++ src/ansys/acp/core/_tree_objects/__init__.py
@@ -1,5 +1,6 @@
 from .element_set import ElementSet
 from .fabric import Fabric
+from .material import Material
 from .model import Model
 from .modeling_group import ModelingGroup
 from .modeling_ply import ModelingPly
@@ -8,6 +9,7 @@ from .rosette import Rosette
 
 __all__ = [
     "Model",
+    "Fabric",
     "ElementSet",
     "Rosette",
     "Fabric",
