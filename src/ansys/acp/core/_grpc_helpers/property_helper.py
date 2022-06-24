7e6bea525dc76906713b4ba6e76142f7bf269671
Jan von Rickenbach <jan.vonrickenbach@ansys.com>
2022-06-24 10:42:53 +0200

    Implement PR comments

--- src/ansys/acp/core/_grpc_helpers/property_helper.py
+++ src/ansys/acp/core/_grpc_helpers/property_helper.py
@@ -25,6 +25,8 @@ def grpc_linked_object_getter(name: str) -> Callable[[TreeObject], Any]:
     """
 
     def inner(self: TreeObject) -> Any:
+        #  Import here to avoid circular references. Cannot use the registry before
+        #  all the object have been imported.
         from .._tree_objects.object_registry import object_registry
 
         if not self._is_stored:
@@ -34,6 +36,9 @@ def grpc_linked_object_getter(name: str) -> Callable[[TreeObject], Any]:
         )
         object_resource_path = _get_data_attribute(self._pb_object, name)
 
+        # Resource path represents an object that is not set as an empty string
+        if object_resource_path == "":
+            return None
         resource_type = object_resource_path.value.split("/")[::2][-1]
         resource_class = object_registry[resource_type]
 
@@ -137,7 +142,10 @@ def grpc_link_property(name: str) -> Any:
     makes call to the gRPC Get endpoints to get the linked object
     """
     return property(grpc_linked_object_getter(name)).setter(
-        grpc_data_setter(name=name, to_protobuf=lambda obj: obj._resource_path)
+        # Resource path represents an object that is not set as an empty string
+        grpc_data_setter(
+            name=name, to_protobuf=lambda obj: "" if obj is None else obj._resource_path
+        )
     )
 
 
