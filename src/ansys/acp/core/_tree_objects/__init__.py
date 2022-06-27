edf2128bb5d01df6436aacfbdda6cd0e2a6e9c24
Dominik Gresch <dominik.gresch@ansys.com>
2022-06-27 09:03:37 +0200

    Add OSS to API Reference

--- src/ansys/acp/core/_tree_objects/__init__.py
+++ src/ansys/acp/core/_tree_objects/__init__.py
@@ -1,6 +1,7 @@
 from .element_set import ElementSet
 from .model import Model
 from .modeling_group import ModelingGroup
+from .oriented_selection_set import OrientedSelectionSet
 from .rosette import Rosette
 
-__all__ = ["Model", "ElementSet", "ModelingGroup", "Rosette"]
+__all__ = ["Model", "ElementSet", "Rosette", "OrientedSelectionSet", "ModelingGroup"]
