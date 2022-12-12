60e6dca9faf7e4c241088dcb4ffa762e2b543b41
Dominik Gresch <dominik.gresch@ansys.com>
2022-12-12 17:24:10 +0100

    Fix import of Protocol

--- src/ansys/acp/core/_server/common.py
+++ src/ansys/acp/core/_server/common.py
@@ -1,5 +1,10 @@
 import enum
-from typing import Dict, Protocol
+from typing import Dict
+
+try:
+    from typing import Protocol
+except ImportError:
+    from typing_extensions import Protocol  # type: ignore
 
 import grpc
 
