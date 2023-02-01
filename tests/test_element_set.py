4671dee18e9591a4d993494152b7b2b89cb02a47
Dominik Gresch <dominik.gresch@ansys.com>
2023-02-01 23:07:32 +0100

    Convert Element Set tests, add missing status

--- tests/test_element_set.py
+++ tests/test_element_set.py
@@ -1,54 +1,41 @@
 import pytest
 
+from common.tree_object_tester import ObjectProperties, TreeObjectTester, WithLockedMixin
 
-def test_create_element_set(load_model_from_tempfile):
-    """Test the creation of Element Sets."""
+
+@pytest.fixture
+def parent_object(load_model_from_tempfile):
     with load_model_from_tempfile() as model:
-        eset_names = ["ElementSet.1", "ElementSet.1", "üñıçよð€"]
-        for ref_name in eset_names:
-            element_set = model.create_element_set(name=ref_name)
-            assert element_set.name == ref_name
+        yield model
 
 
-def test_collection_access(load_model_from_tempfile):
-    """Basic test of the Model.element_sets collection."""
-    with load_model_from_tempfile() as model:
-        initial_num_esets = len(model.element_sets)
-        assert initial_num_esets == 1
+@pytest.fixture
+def tree_object(parent_object):
+    return parent_object.create_element_set()
 
-        eset_names = ["ElementSet.1", "ElementSet.1", "üñıçよð€"]
-        eset_ids = []
-        for ref_name in eset_names:
-            element_set = model.create_element_set(name=ref_name)
-            eset_ids.append(element_set.id)
 
-        assert len(model.element_sets) == len(eset_names) + initial_num_esets
+@pytest.fixture
+def object_properties():
+    return ObjectProperties(
+        read_write={
+            "name": "new_name",
+            "middle_offset": True,
+            "element_labels": (1, 2, 3, 4),
+        },
+        read_only={
+            "id": "some_id",
+            "status": "UPTODATE",
+        },
+    )
 
-        for name in eset_names:
-            assert name in [eset.name for eset in model.element_sets.values()]
 
-        for id in eset_ids:
-            assert id in model.element_sets
-            assert id in model.element_sets.keys()
-
-        eset_names_fromitems = []
-        eset_ids_fromitems = []
-        for key, value in model.element_sets.items():
-            eset_names_fromitems.append(value.name)
-            eset_ids_fromitems.append(key)
-            assert key == value.id
-
-        for name in eset_names:
-            assert name in eset_names_fromitems
-        for id in eset_ids:
-            assert id in eset_ids_fromitems
-
-        REF_ID = eset_ids[0]
-        INEXISTENT_ID = "Inexistent ID"
-
-        assert model.element_sets[REF_ID].id == REF_ID
-        assert model.element_sets.get(REF_ID).id == REF_ID
-
-        with pytest.raises(KeyError):
-            model.element_sets[INEXISTENT_ID]
-        assert model.element_sets.get(INEXISTENT_ID) is None
+class TestElementSet(WithLockedMixin, TreeObjectTester):
+    COLLECTION_NAME = "element_sets"
+    DEFAULT_PROPERTIES = {
+        "status": "NOTUPTODATE",
+        "locked": False,
+        "middle_offset": False,
+        "element_labels": tuple(),
+    }
+    CREATE_METHOD_NAME = "create_element_set"
+    INITIAL_OBJECT_NAMES = ("All_Elements",)
