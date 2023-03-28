56e09d9e6ecca629b2329dff46e40278b09a5d8c
Dominik Gresch <greschd@users.noreply.github.com>
2023-03-28 12:53:55 +0200

    Fix path to versions.json file (#159)

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -49,7 +49,7 @@ html_theme_options = {
     "show_breadcrumbs": True,
     "additional_breadcrumbs": [("PyAnsys", "https://docs.pyansys.com/")],
     "switcher": {
-        "json_url": f"https://{cname}/release/versions.json",
+        "json_url": f"https://{cname}/versions.json",
         "version_match": get_version_match(__version__),
     },
     "check_switcher": False,
