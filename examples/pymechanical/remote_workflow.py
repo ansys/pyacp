df06fff5e12b447f0a85c0b5b210a45409d1ee68
jvonrick <jan.vonrickenbach@ansys.com>
2023-07-19 09:40:42 +0200

    Minor improvements and more comments

--- examples/pymechanical/remote_workflow.py
+++ examples/pymechanical/remote_workflow.py
@@ -1,4 +1,5 @@
 import pathlib
+import os
 
 import ansys.mechanical.core as pymechanical
 from ansys.dpf.composites.composite_model import CompositeModel
@@ -15,11 +16,12 @@ from ansys.dpf.composites.failure_criteria import (
 )
 from ansys.dpf.composites.server_helpers import connect_to_or_start_server
 
+# Run mechanical docker container: docker run -e ANSYSLMD_LICENSE_FILE=1055@milwinlicense1.win.ansys.com -p 50054:10000 ghcr.io/ansys/mechanical:24.1.0
+#mechanical = pymechanical.launch_mechanical(batch=False, port=50054, start_instance=False)
 mechanical = pymechanical.launch_mechanical(batch=False)
 print(mechanical.project_directory)
 
 result = mechanical.run_python_script_from_file("generate_mesh.py", enable_logging=True)
-
 
 pyacp_server = pyacp.launch_acp()
 pyacp_server.wait(timeout=30)
@@ -78,14 +80,23 @@ mg.create_modeling_ply(
         number_of_layers=1,
         global_ply_nr=0,  # add at the end
     )
-
+mg.create_modeling_ply(
+        name="ply2",
+        ply_material=corecell_81kg_5mm,
+        oriented_selection_sets=[oss],
+        ply_angle=0,
+        number_of_layers=2,
+        global_ply_nr=0,  # add at the end
+    )
 
 ACPH5_FILE = "acp.acph5"
 CDB_FILENAME_OUT = "class40_analysis_model.cdb"
 COMPOSITE_DEFINITIONS_H5 = "ACPCompositeDefinitions.h5"
 MATML_FILE = "materials.xml"
 
-output_path = pathlib.Path(mechanical.project_directory)
+SETUP_FOLDER_NAME = "Setup"
+output_path = pathlib.Path(mechanical.project_directory) / SETUP_FOLDER_NAME
+os.mkdir(output_path)
 
 # %%
 # Update and Save the ACP model
@@ -99,22 +110,29 @@ material_output_path = str((output_path / MATML_FILE).resolve())
 import_material_cmd = f"Model.Materials.Import('{material_output_path}')"
 result = mechanical.run_python_script(import_material_cmd)
 
+# The composite definitions file needs to be copied into
+# a folder from which a relative path to the solver files directory
+# can be constructed. Otherwise, the material assignment fails.
+# The prefix before the filename and :: is necessary, but it looks like the actual value is
+# ignored.
+# The second argument for Import is probably the list of mapping files. It is required
+# to pass an empty container if no mapping files are present, otherwise Import will fail.
 import_plies_str = f"""
 clr.AddReference("Ansys.Common.Interop.241")
 external_model_type=Ansys.Common.Interop.DSObjectTypes.DSExternalEnhancedModelType.kEXTERNAL_ENHANCEDMODEL_ASSEMBLEDLAYEREDSECTION
 
-m=Model.InternalObject
-em= m.AddExternalEnhancedModel(external_model_type)
+internal_model=Model.InternalObject
+external_model = internal_model.AddExternalEnhancedModel(external_model_type)
 
 str_cont = Ansys.Common.Interop.AnsCoreObjects.AnsBSTRColl()
-str_cont.Add(r"System::{str((output_path / COMPOSITE_DEFINITIONS_H5).resolve())}")
+str_cont.Add(r"{SETUP_FOLDER_NAME}::{str((output_path / COMPOSITE_DEFINITIONS_H5).resolve())}")
 
 null_cont = Ansys.Common.Interop.AnsCoreObjects.AnsVARIANTColl()
 null_cont.Add(None)
 
-em.Import(str_cont, null_cont)
+external_model.Import(str_cont, null_cont)
 
-em.Update()
+external_model.Update()
     """
 
 result = mechanical.run_python_script(import_plies_str)
@@ -126,8 +144,8 @@ result = mechanical.run_python_script("Model.Analyses[0].Solution.Solve(True)")
 print(mechanical.list_files())
 print(mechanical.project_directory)
 
-rst_file = [filename for filename in  mechanical.list_files() if filename.endswith(".rst")][0]
-matml_out = [filename for filename in  mechanical.list_files() if filename.endswith("MatML.xml")][0]
+rst_file = [filename for filename in mechanical.list_files() if filename.endswith(".rst")][0]
+matml_out = [filename for filename in mechanical.list_files() if filename.endswith("MatML.xml")][0]
 
 dpf_server = connect_to_or_start_server(ip="127.0.0.1", port=50052)
 
@@ -155,4 +173,6 @@ output_all_elements = composite_model.evaluate_failure_criteria(cfc)
 # %%
 # Query and plot the results
 irf_field = output_all_elements.get_field({"failure_label": FailureOutput.FAILURE_VALUE})
+
+assert composite_model.get_element_info(1).n_layers == 3
 irf_field.plot()
