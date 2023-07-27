5ca0047cc1a391dc8c14ec1a2c3cef20341efc0e
jvonrick <jan.vonrickenbach@ansys.com>
2023-07-27 15:39:50 +0200

    Fix type.

--- src/ansys/acp/core/_tree_objects/material/property_sets/wrapper.py
+++ src/ansys/acp/core/_tree_objects/material/property_sets/wrapper.py
@@ -8,7 +8,7 @@ from __future__ import annotations
 from typing import Any, TypeVar
 
 from ..._grpc_helpers.property_helper import _exposed_grpc_property
-from ..._grpc_helpers.protocols import Editable
+from ..._grpc_helpers.protocols import Editable, Gettable
 from .base import _ConstantPropertySet, _PolymorphicMixin, _VariablePropertySet
 
 TC = TypeVar("TC", bound=_ConstantPropertySet)
@@ -24,7 +24,7 @@ def _property_set_getter(name: str, type_constant: type[TC], type_variable: type
     )
     is_monomorphic = not issubclass(type_constant, _PolymorphicMixin)
 
-    def inner(self: Editable) -> TC | TV | None:
+    def inner(self: Gettable) -> TC | TV | None:
         self._get_if_stored()
         if is_monomorphic:
             if not self._pb_object.properties.property_sets.HasField(name):
