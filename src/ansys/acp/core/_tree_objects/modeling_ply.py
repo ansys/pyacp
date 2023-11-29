d431dc75823fe14c7c755ca7886c04db8fc34ca8
Dominik Gresch <dominik.gresch@ansys.com>
2023-11-29 13:30:19 +0100

    Run pre-commit hooks

--- src/ansys/acp/core/_tree_objects/modeling_ply.py
+++ src/ansys/acp/core/_tree_objects/modeling_ply.py
@@ -1,8 +1,7 @@
 from __future__ import annotations
 
-from collections.abc import Container, Iterable
+from collections.abc import Iterable
 import dataclasses
-from typing import Iterable
 
 import numpy as np
 import numpy.typing as npt
