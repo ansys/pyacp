3962813534f08107a03ad76449d62c8c1a091865
Dominik Gresch <dominik.gresch@ansys.com>
2023-12-11 10:14:46 +0100

    Switch to StrEnum for Python 3.11+

--- src/ansys/acp/core/_server/common.py
+++ src/ansys/acp/core/_server/common.py
@@ -3,13 +3,24 @@ from typing import Optional, Protocol
 
 import grpc
 
+try:
+    from enum import StrEnum  # type: ignore
+except ImportError:
+    # For Python 3.10 and below, emulate the behavior of StrEnum by
+    # inheriting from str and enum.Enum.
+    # Note that this does *not* work on Python 3.11+, since the default
+    # Enum format method has changed and will not return the value of
+    # the enum member.
+    class StrEnum(str, enum.Enum):  # type: ignore
+        pass
 
-class ServerKey(str, enum.Enum):
+
+class ServerKey(StrEnum):  # type: ignore
     MAIN = "main"
     FILE_TRANSFER = "file_transfer"
 
 
-class LaunchMode(str, enum.Enum):
+class LaunchMode(StrEnum):  # type: ignore
     DIRECT = "direct"
     DOCKER_COMPOSE = "docker_compose"
 
