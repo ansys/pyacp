40f54441728d694f2a82d13fba08d15545e85114
jvonrick <jan.vonrickenbach@ansys.com>
2024-01-23 14:30:56 +0100

    Initial prototypes for workflow and examples

---
+++ src/ansys/acp/core/model_printer.py
@@ -0,0 +1,51 @@
+from ansys.acp.core import Model
+
+
+class Node:
+    def __init__(self, value, children=None):
+        self.value = value
+        self.children = children if children else []
+
+    def __str__(self, level=0):
+        ret = "\t" * level + repr(self.value) + "\n"
+        for child in self.children:
+            ret += child.__str__(level + 1)
+        return ret
+
+
+def _add_tree_part(
+    tree: Node,
+    container_name: str,
+    model: Model,
+):
+    container = Node(container_name)
+    tree.children.append(container)
+    for entity_name, entity in getattr(model, container_name).items():
+        group_node = Node(entity_name)
+        container.children.append(group_node)
+
+
+def print_model(model: Model):
+    tree = Node("Model")
+
+    _add_tree_part(tree, "element_sets", model)
+    _add_tree_part(tree, "materials", model)
+    _add_tree_part(tree, "fabrics", model)
+    _add_tree_part(tree, "oriented_selection_sets", model)
+
+    modeling_groups = Node("Modeling Groups")
+    tree.children.append(modeling_groups)
+    for modeling_group_name, modeling_group in model.modeling_groups.items():
+        group_node = Node(modeling_group_name)
+        modeling_groups.children.append(group_node)
+        for modeling_ply_name, modeling_ply in modeling_group.modeling_plies.items():
+            modeling_ply_node = Node(modeling_ply_name)
+            group_node.children.append(modeling_ply_node)
+            for production_ply_name, production_ply in modeling_ply.production_plies.items():
+                production_ply_node = Node(production_ply_name)
+                modeling_ply_node.children.append(production_ply_node)
+                for analysis_ply_name, analysis_ply in production_ply.analysis_plies.items():
+                    analysis_ply_node = Node(analysis_ply_name)
+                    production_ply_node.children.append(analysis_ply_node)
+
+    print(tree)
--- src/ansys/acp/core/workflow.py
+++ src/ansys/acp/core/workflow.py
@@ -14,7 +14,7 @@ class LocalFileStrategy:
     def __init__(self, local_working_directory: pathlib.Path):
         self._local_working_directory = local_working_directory
 
-    def get_file(self, get_file_callable: Callable[[None], pathlib.Path], filename: str):
+    def get_file(self, get_file_callable: Callable[[pathlib.Path], None], filename: str):
         local_path = self._local_working_directory / filename
         get_file_callable(self._local_working_directory / filename)
         return local_path
@@ -33,11 +33,6 @@ class RemoteFileStrategy:
 
 
 class ACPWorkflow:
-    # Todo: We need to always upload referenced files (e.g. geometries) to the server
-    # Read linked file paths from acph5 file if they exist:
-    #    * cdb
-    #    * geometries (upload only needed if a refresh is requested)
-    #    * others?
     def __init__(
         self,
         acp_client: Client,
@@ -46,13 +41,11 @@ class ACPWorkflow:
     ):
         self._acp_client = acp_client
 
-        # todo copy to temp directory if it is used
         if cdb_file_path is not None:
             uploaded_file = self._acp_client.upload_file(local_path=cdb_file_path)
             if h5_file_path is None:
                 self._model = self._acp_client.import_model(path=uploaded_file, format="ansys:cdb")
 
-        # todo copy to temp directory if it is used
         if h5_file_path is not None:
             uploaded_file = self._acp_client.upload_file(local_path=h5_file_path)
             self._model = self._acp_client.import_model(path=uploaded_file)
@@ -86,54 +79,6 @@ class ACPWorkflow:
 
     def get_local_acp_h5(self):
         return self._file_strategy.get_file(self._model.save, "model.acph5")
-
-
-class Node:
-    def __init__(self, value, children=None):
-        self.value = value
-        self.children = children if children else []
-
-    def __str__(self, level=0):
-        ret = "\t" * level + repr(self.value) + "\n"
-        for child in self.children:
-            ret += child.__str__(level + 1)
-        return ret
-
-
-def print_model(model):
-    tree = Node("Model")
-
-    def add_tree_part(
-        tree,
-        name,
-    ):
-        entities = Node(name)
-        tree.children.append(entities)
-        for entity_name, entity in getattr(model, name).items():
-            group_node = Node(entity_name)
-            entities.children.append(group_node)
-
-    add_tree_part(tree, "element_sets")
-    add_tree_part(tree, "materials")
-    add_tree_part(tree, "fabrics")
-    add_tree_part(tree, "oriented_selection_sets")
-
-    modeling_groups = Node("Modeling Groups")
-    tree.children.append(modeling_groups)
-    for modeling_group_name, modeling_group in model.modeling_groups.items():
-        group_node = Node(modeling_group_name)
-        modeling_groups.children.append(group_node)
-        for modeling_ply_name, modeling_ply in modeling_group.modeling_plies.items():
-            modeling_ply_node = Node(modeling_ply_name)
-            group_node.children.append(modeling_ply_node)
-            for production_ply_name, production_ply in modeling_ply.production_plies.items():
-                production_ply_node = Node(production_ply_name)
-                modeling_ply_node.children.append(production_ply_node)
-                for analysis_ply_name, analysis_ply in production_ply.analysis_plies.items():
-                    analysis_ply_node = Node(analysis_ply_name)
-                    production_ply_node.children.append(analysis_ply_node)
-
-    print(tree)
 
 
 def get_composite_post_processing_files(acp_files: ACPWorkflow, local_rst_file_path: PATH):
