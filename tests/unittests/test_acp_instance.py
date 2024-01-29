5a91f23ccbcc25a59be3af4c6b52e1ba1994da65
Dominik Gresch <dominik.gresch@ansys.com>
2024-01-29 15:43:38 +0100

    Add property to get all models loaded on the server

--- tests/unittests/test_acp_instance.py
+++ tests/unittests/test_acp_instance.py
@@ -2,3 +2,17 @@ def test_server_version(acp_instance):
     version = acp_instance.server_version
     assert isinstance(version, str)
     assert version != ""
+
+def test_models(acp_instance, load_model_from_tempfile):
+    with load_model_from_tempfile() as m1:
+        assert acp_instance.models == (m1,)
+
+        with load_model_from_tempfile() as m2:
+            # the order of models is not guaranteed
+            current_models = acp_instance.models
+            assert len(current_models) == 2
+            assert m1 in current_models
+            assert m2 in current_models
+
+            acp_instance.clear()
+            assert acp_instance.models == ()
