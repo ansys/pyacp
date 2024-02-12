50d5daed6f4e6215f2ba135d645da7cbdebe4677
Dominik Gresch <dominik.gresch@ansys.com>
2024-02-12 11:58:12 +0100

    Add object permanence tests for container types

--- tests/unittests/test_object_permanence.py
+++ tests/unittests/test_object_permanence.py
@@ -1,36 +1,81 @@
 import gc
 
+import pytest
+
 import ansys.acp.core as pyacp
+from ansys.acp.core._tree_objects._grpc_helpers.edge_property_list import EdgePropertyList
+from ansys.acp.core._tree_objects._grpc_helpers.linked_object_list import LinkedObjectList
+from ansys.acp.core._tree_objects._grpc_helpers.mapping import Mapping, MutableMapping
 
 
-def test_object_identity(load_model_from_tempfile):
+@pytest.fixture
+def model(load_model_from_tempfile):
     with load_model_from_tempfile() as model:
-        mg_0 = model.create_modeling_group()
-        mg_1 = model.modeling_groups[mg_0.id]
-        mg_2 = model.modeling_groups[mg_0.id]
-        assert mg_0 is mg_1 is mg_2
+        yield model
 
 
-def test_object_identity_after_deletion(load_model_from_tempfile):
+def test_object_identity(model):
+    mg_0 = model.create_modeling_group()
+    mg_1 = model.modeling_groups[mg_0.id]
+    mg_2 = model.modeling_groups[mg_0.id]
+    assert mg_0 is mg_1 is mg_2
+
+
+def test_object_identity_after_deletion(model):
     """Check that objects are deleted when no longer referenced."""
-    with load_model_from_tempfile() as model:
-        key = list(model.modeling_groups.keys())[0]
+    key = list(model.modeling_groups.keys())[0]
 
-        # Immediately consume the objects via 'id' to ensure no references
-        # are kept by the test infrastructure.
-        id1 = id(model.modeling_groups[key])
-        gc.collect()
-        id2 = id(model.modeling_groups[key])
-        assert id1 != id2
+    # Immediately consume the objects via 'id' to ensure no references
+    # are kept by the test infrastructure.
+    id1 = id(model.modeling_groups[key])
+    gc.collect()
+    id2 = id(model.modeling_groups[key])
+    assert id1 != id2
 
-        # test the inverse: keep a reference alive explicitly
-        _ = model.modeling_groups[key]
-        id1 = id(model.modeling_groups[key])
-        gc.collect()
-        id2 = id(model.modeling_groups[key])
-        assert id1 == id2
+    # test the inverse: keep a reference alive explicitly
+    _ = model.modeling_groups[key]
+    id1 = id(model.modeling_groups[key])
+    gc.collect()
+    id2 = id(model.modeling_groups[key])
+    assert id1 == id2
 
 
 def test_unstored():
     """Check that unstored objects have unique identities."""
     assert pyacp.ModelingPly() is not pyacp.ModelingPly()  # type: ignore
+
+
+def test_mapping_identity(model):
+    """Check that Mapping objects have the same ID when accessed twice."""
+    modeling_ply = list(list(model.modeling_groups.values())[0].modeling_plies.values())[0]
+    production_ply_mapping_1 = modeling_ply.production_plies
+    production_ply_mapping_2 = modeling_ply.production_plies
+    assert isinstance(production_ply_mapping_1, Mapping)
+    assert not isinstance(production_ply_mapping_1, MutableMapping)
+    assert production_ply_mapping_1 is production_ply_mapping_2
+
+
+def test_mutable_mapping_identity(model):
+    """Check that MutableMapping objects have the same ID when accessed twice."""
+    mg_mapping_1 = model.modeling_groups
+    mg_mapping_2 = model.modeling_groups
+    assert isinstance(mg_mapping_1, MutableMapping)
+    assert mg_mapping_1 is mg_mapping_2
+
+
+def test_linked_object_list_identity(model):
+    """Check that LinkedObjectList objects have the same ID when accessed twice."""
+    oss = list(model.oriented_selection_sets.values())[0]
+    element_sets_list_1 = oss.element_sets
+    element_sets_list_2 = oss.element_sets
+    assert isinstance(element_sets_list_1, LinkedObjectList)
+    assert element_sets_list_1 is element_sets_list_2
+
+
+def test_edge_property_list_identity(model):
+    """Check that EdgePropertyList objects have the same ID when accessed twice."""
+    modeling_ply = list(list(model.modeling_groups.values())[0].modeling_plies.values())[0]
+    selection_rules_1 = modeling_ply.selection_rules
+    selection_rules_2 = modeling_ply.selection_rules
+    assert isinstance(selection_rules_1, EdgePropertyList)
+    assert selection_rules_1 is selection_rules_2
