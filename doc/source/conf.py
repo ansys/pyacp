d09af9d1dc6213d7d6ad95d816ae97265a33b6be
Dominik Gresch <dominik.gresch@ansys.com>
2024-02-26 15:52:43 +0100

    Reenable warning as error on quick doc build
    Add back the '-W' flag to the 'quick' doc build, by manually
    creating a placeholder reference for interlinked gallery examples
    which don't exist.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -25,8 +25,6 @@ SKIP_GALLERY = os.environ.get("PYACP_DOC_SKIP_GALLERY", "0").lower() in ("1", "t
 SKIP_API = os.environ.get("PYACP_DOC_SKIP_API", "0").lower() in ("1", "true")
 
 exclude_patterns = []
-if SKIP_GALLERY:
-    exclude_patterns.append("examples/*")
 if SKIP_API:
     exclude_patterns.append("api/*")
 
