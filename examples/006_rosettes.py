41bd20defbfac5499ff31ca27a836e0ed59921ed
janvonrickenbach <jan.vonrickenbach@ansys.com>
2024-03-04 10:10:47 +0100

    Update examples/006_rosettes.py
    Co-authored-by: Kathy Pippert <84872299+PipKat@users.noreply.github.com>

--- examples/006_rosettes.py
+++ examples/006_rosettes.py
@@ -45,8 +45,8 @@ input_file = get_example_file(ExampleKeys.BASIC_FLAT_PLATE_DAT, WORKING_DIR)
 acp = launch_acp()
 
 # %%
-# Define the input file and instantiate an ACPWorkflow.
-# The ACPWorkflow class provides convenience methods which simplify the file handling.
+# Define the input file and instantiate an ACP workflow.
+# The ``ACPWorkflow`` class provides convenience methods that simplify the file handling.
 # It automatically creates a model based on the input file.
 # The input file contains a flat plate with a single ply.
 workflow = ACPWorkflow.from_cdb_or_dat_file(
