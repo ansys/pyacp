4ce9c95579a45a1e2f877f8a939b22f5367a10be
Nellie Shum <133802577+nshum4@users.noreply.github.com>
2024-03-25 09:11:39 -0400

    Update examples/001_basic_flat_plate.py
    Co-authored-by: Kathy Pippert <84872299+PipKat@users.noreply.github.com>

--- examples/001_basic_flat_plate.py
+++ examples/001_basic_flat_plate.py
@@ -231,7 +231,7 @@ mapdl.download(rstfile_name, str(workflow.working_directory.path))
 # -----------------------------------
 #
 # To postprocess the results, you must configure the imports, connect to the
-# PyDPF Composites server and load its plugin.
+# PyDPF Composites server, and load its plugin.
 
 from ansys.dpf.composites.composite_model import CompositeModel
 from ansys.dpf.composites.constants import FailureOutput
