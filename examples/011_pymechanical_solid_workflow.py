2fcd4d4bb13ba0f42ae70dc29b4e334a1dc10975
Dominik Gresch <dominik.gresch@ansys.com>
2024-11-18 14:11:01 +0100

    Launch PyMechanical in batch mode

--- examples/011_pymechanical_solid_workflow.py
+++ examples/011_pymechanical_solid_workflow.py
@@ -83,7 +83,7 @@ import ansys.mechanical.core as pymechanical
 with ThreadPoolExecutor() as executor:
     futures = [
         executor.submit(pymechanical.launch_mechanical, batch=True),
-        executor.submit(pymechanical.launch_mechanical, batch=False),
+        executor.submit(pymechanical.launch_mechanical, batch=True),
         executor.submit(pyacp.launch_acp),
         executor.submit(pydpf_composites.server_helpers.connect_to_or_start_server),
     ]
