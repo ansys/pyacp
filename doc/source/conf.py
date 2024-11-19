0350a1a83ee2eac52346232d0f89c9122b4eda2a
Dominik Gresch <dominik.gresch@ansys.com>
2024-11-19 16:33:47 +0100

    Remove duplicate config keys

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -275,18 +275,14 @@ sphinx_gallery_conf = {
     # Sort gallery example by filename instead of number of lines (default)
     "within_subsection_order": "FileNameSortKey",
     # directory where function granular galleries are stored
-    "backreferences_dir": None,
-    # Modules for which function level galleries are created.  In
-    "doc_module": "ansys-acp-core",
+    "backreferences_dir": "api/_gallery_backreferences",
+    # Modules for which function level galleries are created.
+    "doc_module": ("ansys.acp.core"),
+    "exclude_implicit_doc": {"ansys\\.acp\\.core\\._.*"},  # ignore private submodules
     "image_scrapers": (DynamicScraper(), "matplotlib"),
     "ignore_pattern": r"__init__\.py",
     "thumbnail_size": (350, 350),
     "remove_config_comments": True,
-    # directory where function/class granular galleries are stored
-    "backreferences_dir": "api/_gallery_backreferences",
-    # Modules for which function/class level galleries are created.
-    "doc_module": ("ansys.acp.core"),
-    "exclude_implicit_doc": {"ansys\\.acp\\.core\\._.*"},  # ignore private submodules
 }
 
 # Favicon
