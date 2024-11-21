6d8d8fd6edbba8d5fe107d51c856629181c40d6c
Dominik Gresch <dominik.gresch@ansys.com>
2024-11-21 11:17:25 +0100

    Update the advanced PyMAPDL example
    Update the advanced PyMAPDL (class40) example:
    - Download the input file from the ``example_data`` repository
      instead of relying on a file in the PyACP repository
    - Use temporary local working directories, to avoid polluting the
      local directory.
    - Test that the example runs with a local install, and remove
      the note about Docker containers.
      Note that this is not checked continuously in CI.

--- src/ansys/acp/core/extras/example_helpers.py
+++ src/ansys/acp/core/extras/example_helpers.py
@@ -65,6 +65,7 @@ class ExampleKeys(Enum):
     MINIMAL_FLAT_PLATE = auto()
     OPTIMIZATION_EXAMPLE_DAT = auto()
     CLASS40_AGDB = auto()
+    CLASS40_CDB = auto()
     MATERIALS_XML = auto()
 
 
@@ -97,6 +98,7 @@ EXAMPLE_FILES: dict[ExampleKeys, _ExampleLocation] = {
         directory="optimization_example", filename="optimization_model.dat"
     ),
     ExampleKeys.CLASS40_AGDB: _ExampleLocation(directory="class40", filename="class40.agdb"),
+    ExampleKeys.CLASS40_CDB: _ExampleLocation(directory="class40", filename="class40.cdb"),
     ExampleKeys.MATERIALS_XML: _ExampleLocation(directory="materials", filename="materials.engd"),
 }
 
