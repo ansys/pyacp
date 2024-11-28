d440904ba14e2ade888e3a901a216d8e43f025b0
Dominik Gresch <greschd@users.noreply.github.com>
2024-11-28 09:35:44 +0100

    Fix tree printer to include all object types (#679)
    Change the implementation of 'get_model_tree' to use the '_GRPC_PROPERTIES' class attribute, and thus include all newly-added object types in the representation.
    
    The style of the tree has changed in some ways:
    - The root node now shows the model name, instead of always 'Model'
    - Objects (name or id) are distinguished from collections by wrapping the string in single quotes
    - The additional nesting level for 'Materials', 'Selection Rules', and other 'logical' groupings which are not part of the PyACP hierarchy has been removed
    
    The new tree structure can be seen in the updated test cases, or in `print_model.rst`.
    
    Partially addresses #348, interactive tree support in Jupyter notebooks is still missing.

--- src/ansys/acp/core/_model_printer.py
+++ src/ansys/acp/core/_model_printer.py
@@ -22,6 +22,8 @@
 
 import os
 
+from ._tree_objects._grpc_helpers.mapping import Mapping
+from ._tree_objects.base import TreeObjectBase
 from ._tree_objects.model import Model
 from ._utils.string_manipulation import replace_underscores_and_capitalize
 
@@ -52,95 +54,56 @@ class Node:
         return ret
 
 
-def _add_tree_part(
-    tree: Node,
-    container_name: str,
-    model: Model,
-) -> None:
-    items = list(getattr(model, container_name).items())
-    if len(items) == 0:
-        return
-    container = Node(replace_underscores_and_capitalize(container_name))
-    tree.children.append(container)
-    for entity_name, entity in items:
-        group_node = Node(entity_name)
-        container.children.append(group_node)
-
-
-def print_model(model: Model) -> None:
+def print_model(model: Model, *, hide_empty: bool = True) -> None:
     """Print a tree representation of the model.
 
     Parameters
     ----------
     model:
         pyACP model
+    hide_empty :
+        Whether to hide empty collections.
 
     """
-    return print(get_model_tree(model))
+    return print(get_model_tree(model, hide_empty=hide_empty))
 
 
-def get_model_tree(model: Model) -> Node:
+def get_model_tree(model: Model, *, hide_empty: bool = True) -> Node:
     """Get a tree representation of the model.
 
     Returns the root node.
 
     Parameters
     ----------
-    model:
-        pyACP model.
+    model :
+        ACP model.
+    hide_empty :
+        Whether to hide empty collections.
     """
-    model_node = Node("Model")
+    return _get_model_tree_impl(obj=model, hide_empty=hide_empty)
 
-    material_data = Node("Material Data")
-    model_node.children.append(material_data)
-    _add_tree_part(material_data, "materials", model)
-    _add_tree_part(material_data, "fabrics", model)
-    _add_tree_part(material_data, "stackups", model)
-    _add_tree_part(material_data, "sublaminates", model)
 
-    _add_tree_part(model_node, "element_sets", model)
-    _add_tree_part(model_node, "edge_sets", model)
+def _get_model_tree_impl(obj: TreeObjectBase, *, hide_empty: bool) -> Node:
+    obj_node = Node(repr(_name_or_id(obj)))
+    for attr_name in obj._GRPC_PROPERTIES:
+        try:
+            attr = getattr(obj, attr_name)
+        except (AttributeError, RuntimeError):
+            continue
+        if isinstance(attr, Mapping):
+            collection_node = Node(replace_underscores_and_capitalize(attr_name))
+            obj_node.children.append(collection_node)
+            for child_obj in attr.values():
+                collection_node.children.append(
+                    _get_model_tree_impl(child_obj, hide_empty=hide_empty)
+                )
+            if hide_empty and not collection_node.children:
+                obj_node.children.pop()
+    return obj_node
 
-    geometry = Node("Geometry")
-    model_node.children.append(geometry)
-    _add_tree_part(geometry, "cad_geometries", model)
-    _add_tree_part(geometry, "virtual_geometries", model)
 
-    _add_tree_part(model_node, "rosettes", model)
-
-    lookup_table = Node("Lookup Tables")
-    model_node.children.append(lookup_table)
-    _add_tree_part(lookup_table, "lookup_tables_1d", model)
-    _add_tree_part(lookup_table, "lookup_tables_3d", model)
-
-    selection_rules = Node("Selection Rules")
-    model_node.children.append(selection_rules)
-    _add_tree_part(selection_rules, "parallel_selection_rules", model)
-    _add_tree_part(selection_rules, "cylindrical_selection_rules", model)
-    _add_tree_part(selection_rules, "spherical_selection_rules", model)
-    _add_tree_part(selection_rules, "tube_selection_rules", model)
-    _add_tree_part(selection_rules, "cutoff_selection_rules", model)
-    _add_tree_part(selection_rules, "geometrical_selection_rules", model)
-    _add_tree_part(selection_rules, "variable_offset_selection_rules", model)
-    _add_tree_part(selection_rules, "boolean_selection_rules", model)
-
-    _add_tree_part(model_node, "oriented_selection_sets", model)
-
-    modeling_groups = Node("Modeling Groups")
-    model_node.children.append(modeling_groups)
-    for modeling_group_name, modeling_group in model.modeling_groups.items():
-        group_node = Node(modeling_group_name)
-        modeling_groups.children.append(group_node)
-        for modeling_ply_name, modeling_ply in modeling_group.modeling_plies.items():
-            modeling_ply_node = Node(modeling_ply_name)
-            group_node.children.append(modeling_ply_node)
-            for production_ply_name, production_ply in modeling_ply.production_plies.items():
-                production_ply_node = Node(production_ply_name)
-                modeling_ply_node.children.append(production_ply_node)
-                for analysis_ply_name, analysis_ply in production_ply.analysis_plies.items():
-                    analysis_ply_node = Node(analysis_ply_name)
-                    production_ply_node.children.append(analysis_ply_node)
-
-    _add_tree_part(model_node, "sensors", model)
-
-    return model_node
+def _name_or_id(obj: TreeObjectBase) -> str:
+    try:
+        return obj.name
+    except AttributeError:
+        return obj.id  # type: ignore
