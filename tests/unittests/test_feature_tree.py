023b59b0595d55c013836c0b3b0bc8c016673e9f
Dominik Gresch <dominik.gresch@ansys.com>
2025-04-17 14:43:58 +0200

    Add a feature tree
    Add helper functions to either print a tree of ACP objects, or
    print it. In the documentation, show the tree structure, and
    explain how it affects the access and creation of objects.
    
    To implement the feature tree, record metadata when creating
    mapping properties (value type, and read-only status), and use
    a separate _exposed_grpc_property subclass to indicate it is
    a mapping.

---
+++ tests/unittests/test_feature_tree.py
@@ -0,0 +1,89 @@
+# Copyright (C) 2022 - 2025 ANSYS, Inc. and/or its affiliates.
+# SPDX-License-Identifier: MIT
+#
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import os
+import textwrap
+
+from ansys.acp.core.extras.feature_tree import get_feature_tree
+
+
+def test_feature_tree():
+    """Test that the feature tree is correct."""
+    tree = get_feature_tree()
+    expected = textwrap.dedent(
+        """\
+        Model
+        ├── Material
+        ├── Fabric
+        ├── Stackup
+        ├── SubLaminate
+        ├── ElementSet
+        ├── EdgeSet
+        ├── CADGeometry
+        │   └── CADComponent (read-only)
+        ├── VirtualGeometry
+        ├── Rosette
+        ├── LookUpTable1D
+        │   └── LookUpTable1DColumn
+        ├── LookUpTable3D
+        │   └── LookUpTable3DColumn
+        ├── ParallelSelectionRule
+        ├── CylindricalSelectionRule
+        ├── SphericalSelectionRule
+        ├── TubeSelectionRule
+        ├── CutOffSelectionRule
+        ├── GeometricalSelectionRule
+        ├── VariableOffsetSelectionRule
+        ├── BooleanSelectionRule
+        ├── OrientedSelectionSet
+        ├── ModelingGroup
+        │   ├── ModelingPly
+        │   │   └── ProductionPly (read-only)
+        │   │       └── AnalysisPly (read-only)
+        │   ├── InterfaceLayer
+        │   └── ButtJointSequence
+        ├── ImportedModelingGroup
+        │   └── ImportedModelingPly
+        │       └── ImportedProductionPly (read-only)
+        │           └── ImportedAnalysisPly (read-only)
+        ├── SamplingPoint
+        ├── SectionCut
+        ├── SolidModel
+        │   ├── ExtrusionGuide
+        │   ├── SnapToGeometry
+        │   ├── SolidElementSet (read-only)
+        │   ├── CutOffGeometry
+        │   ├── AnalysisPly (read-only)
+        │   └── InterfaceLayer (read-only)
+        ├── ImportedSolidModel
+        │   ├── SolidElementSet (read-only)
+        │   ├── CutOffGeometry
+        │   ├── LayupMappingObject
+        │   │   ├── AnalysisPly (read-only)
+        │   │   └── ImportedAnalysisPly (read-only)
+        │   ├── AnalysisPly (read-only)
+        │   └── ImportedAnalysisPly (read-only)
+        ├── Sensor
+        └── FieldDefinition
+        """
+    )
+    assert str(tree) == expected.replace("\n", os.linesep)
