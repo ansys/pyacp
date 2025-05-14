56a314c91703af277b214c7819d97a7446b91af7
Simone Raimondi <simone.raimondi@ansys.com>
2025-05-14 13:43:37 +0200

    Testing

--- tests/unittests/test_lookup_table_3d.py
+++ tests/unittests/test_lookup_table_3d.py
@@ -96,7 +96,7 @@ class TestLookUpTable3D(NoLockedMixin, TreeObjectTester):
             return ObjectPropertiesToTest(
                 read_write=[
                     ("name", "3D Look-Up Table name"),
-                    ("use_global_coordinate_system", False),
+                    ("use_global_coordinate_system", True),
                     ("rosette", rosette),
                     (
                         "interpolation_algorithm",
